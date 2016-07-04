36b2971917270bfcbd1ab5c93e5a90ee1ca1b711
dev <dev@jace>
2016-07-04 16:02:11 +0900

    init.

---
+++ src/main/java/lotto/store/controller/LottoStoreController.java
@@ -0,0 +1,16 @@
+package lotto.store.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/lotto/store")
+public class LottoStoreController {
+	String prefix = "lotto/store/";
+	
+	@RequestMapping(value = "/list")
+	public String getAddCustomerPage(ModelMap model) {
+		return prefix + "list";
+	}
+}
